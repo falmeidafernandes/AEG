ec0329c36cb43e8ce6581afe7755f67e6f3c2023
falmeidafernandes <falmeidafernandes@gmail.com>
2019-04-30 16:10:29 -0300

    'included more methods for the AEG'

--- AEG.py
+++ AEG.py
@@ -5,6 +5,17 @@ import numpy as np
 """
 
 
+# The code must be designed to work like this: (ex. to fit parameters of a line function)
+# >>> import aeg
+# >>> from aeg.Selector import MostFit
+# >>> from aeg.ObjectiveFunction import LineFitting
+# >>>
+# >>> model = aeg.AEG(genome = {'a': [-10,10], 'b': [-10,10]},
+#                     objective_function = LineFitting,
+#                     selector = MostFit)
+# >>>
+# >>> model.iterate(training_data = {'x': x, 'y': y})
+# >>> print model.fittest
 
 class Individual(object):
     
@@ -14,7 +25,7 @@ class Individual(object):
     
     
     def __init__(self, genome, ancestors = None, generation = 0, 
-                 Q_value = np.nan, generation_rank = np.nan):
+                 fitness = np.nan, generation_rank = np.nan):
         
         """
         
@@ -22,7 +33,7 @@ class Individual(object):
         
         self.genome = genome
         self.generation = generation
-        self.Q_value = Q_value
+        self.fitness = fitness
         self.generation_rank = generation_rank
         
         if ancestors is None:
@@ -55,7 +66,7 @@ class Individual(object):
                 elif choosen_partner == 1:
                     param_loc = partner.genome[param]
                 
-                param_sampled = float(np.random.normal(loc = self.genome[param],
+                param_sampled = float(np.random.normal(loc = param_loc,
                                                        scale = mutation_factor,
                                                        size = 1))
                 
@@ -73,7 +84,7 @@ class Individual(object):
 
 teste = Individual(genome = {'a': 1, 'b': 2})
 children = teste.reproduce()
-children[0].genome
+print children[0].genome
 
 
 
@@ -83,10 +94,11 @@ class AEG(object):
     
     """
     
-    def __init__(self, genome, Q_function):
+    def __init__(self, genome, objective_function, selector):
         
         self.genome = genome
-        self.Q_function = Q_function
+        self.objective_function = objective_function
+        self.selector = selector
         self.generation = 0
         self.individuals = []
     
@@ -94,34 +106,90 @@ class AEG(object):
         """
         Orders individuals by Q_value rank
         """
-    
-    def new_generation(self, N_expontaneous, N_survivors,
-                       N_children_per_survivor, expontaneous_genome_range):
+
+    def create_spontaneous_generation(self, N_spontaneous):
+        """
+
+        :param N_spontaneous:
+        :return:
+        """
+
+        new_spontaneous_individuals = []
+        ancestors_spontaneous = [np.nan] * self.generation + [0]
+
+        for i in range(N_spontaneous):
+            genome_i = {}
+            for param in self.genome.keys():
+                vmin = self.genome[param][0]
+                vmax = self.genome[param][1]
+                genome_i[param] = vmin + (vmax - vmin) * np.random.uniform()
+
+            individual = Individual(genome=genome_i,
+                                    ancestors=ancestors_spontaneous)
+
+            new_spontaneous_individuals.append(individual)
+
+        return new_spontaneous_individuals
+
+
+    def create_new_generation(self, N_spontaneous = 1000, survivors = None,
+                              N_children_per_survivor = 1000):
         
         # add new list to self.individuals
         self.individuals.append([])
         
-        # expontaneously generated
-        ancestors_expontaneous = [np.nan]*self.generation + [0]
-        
-        for i in range(N_expontaneous):
-            genome_i = {}
-            for param in self.genome.keys():
-                vmin = expontaneous_genome_range[param][0]
-                vmax = expontaneous_genome_range[param][1]
-                genome_i[param] = vmin + (vmax-vmin)*np.random.uniform()
-            
-            individual = Individual(genome = genome_i,
-                                    ancestors = ancestors_expontaneous)
-            
-            self.individuals[-1].append(individual)
+        # generate spontaneous individuals
+        new_spontaneous_individuals = self.create_spontaneous_generation(N_spontaneous=N_spontaneous)
+        self.individuals[-1] += new_spontaneous_individuals
         
         # survivor's reproduction
-        if self.generation > 0:
-            
-            for i in range(N_survivors):
-                N = N_children_per_survivor
-                children = self.individuals[-2][i].reproduce(N_children = N)
-                self.individuals[-1] = self.individuals[-1] + children
+        if survivors is not None:
+            for survivor in survivors:
+                children = survivor.reproduce(N_children=N_children_per_survivor)
+                self.individuals[-1] += children
+
+        # Update generation number
+        self.generation += 1
     
-    
+    def iterate(self, training_data, max_iterations = 100, fitness_threshold = 0.999, N_spontaneous = 1000, **kargs):
+        """
+
+        :return:
+        """
+
+        # Step 0: generate first spontaneous population
+        self.create_new_generation(N_spontaneous = N_spontaneous)
+
+        # Begin the loop
+        overall_fittest = None
+        finished = False
+        iteration = 0
+
+        while not finished:
+            iteration += 1
+
+            # Step 1: measure fitness of the current generation
+            current_generation_individuals = self.individuals[-1]
+            self.objective_function(current_generation_individuals, training_data)
+
+            # Step 2: select the fittest individuals
+            current_fittest = self.selector(kargs)
+
+            # Step 3: check assignment of overall fittest
+            if overall_fittest is None:
+                overall_fittest = current_fittest[0]
+            else:
+                if current_fittest[0].fitness > overall_fittest.fitness:
+                    overall_fittest = current_fittest[0]
+
+            # Step 4: check if final is achieve
+            if current_fittest[0].fitness >= fitness_threshold:
+                self.fittest = overall_fittest
+                return overall_fittest
+
+            elif iteration == max_iterations:
+                self.fittest = overall_fittest
+                return overall_fittest
+
+            # Step 5: reproduction of new generation
+            self.create_new_generation(N_spontaneous = N_spontaneous, survivors = current_fittest)
